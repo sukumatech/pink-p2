5bff1f676125bcfe51a4b164db014b3e3dbe6c8c
MasoodAhmedRafay1 <32939307+MasoodAhmedRafay1@users.noreply.github.com>
2022-01-24 13:56:23 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('...HELLLOOOOOOOOO World...!');
+  res.end('...PINK GUYS ARE AWESOME...!');
 }).listen(8080, () => console.log('listening'));
