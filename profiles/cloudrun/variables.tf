0abcdc749e5b20a15691dc5852763a1dea80cc86
MasoodAhmedRafay1 <rafayqureshi2010@gmail.com>
2022-01-28 14:39:46 +0100

    prod env related changes

--- profiles/cloudrun/variables.tf
+++ profiles/cloudrun/variables.tf
@@ -23,12 +23,6 @@ variable "location" {
 # default = ["us-central1"]
 }
 
-/*variable "image" {
-  type        = string
-  description = "Docker image name."
-#  default = "gcr.io/myproject-calculator-337809/calcimage:v1"
-}*/
-
 variable "project" {
   type        = string
   description = "Google Cloud project in which to create resources."
